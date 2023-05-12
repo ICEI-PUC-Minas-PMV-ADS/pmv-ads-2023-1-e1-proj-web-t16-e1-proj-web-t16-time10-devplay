10342ef4e59cc87ac72314f462721ed97cc0761a
marcelo.brito <marcelo.brito@g.globo>
2023-05-12 12:23:54 -0300

    Adiciona React ao projeto e algumas rotas de exemplo

---
+++ app/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Root from "./routes/root";
+import Contact from "./routes/contact";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "contacts/:contactId",
+        element: <Contact />,
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
